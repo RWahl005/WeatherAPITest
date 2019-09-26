a5b6ae7980f24188a5600ce5ceb8a76f2fabf9e1
RWahl8177 <rwahl005@west-mec.org>
2019-09-26 14:04:55 -0700

    Finished weekly view.
    Finished weekly view.

---
+++ www/weatherq.js
@@ -0,0 +1,125 @@
+var o = getData();
+var data = `https:\\\\api.weather.gov/points/${o[0][1]},${o[1][1]}`;
+clickToday();
+
+fetch(`https:\\\\api.weather.gov/points/${o[0][1]},${o[1][1]}`).then(e => {
+    return e.json();
+}).then(r =>{
+    console.log(r);
+    getCounty(r);
+    document.getElementById("city").innerHTML = r.properties.relativeLocation.properties.city + ", " + r.properties.relativeLocation.properties.state
+});
+
+function getCounty(r){
+    fetch(r.properties.county).then(e => {
+        return e.json();
+    }).then(r =>{
+        document.getElementById("county").innerHTML = r.properties.name + " County";
+    });
+}
+
+// function displayToday(){
+//     fetch(data).then(e =>{
+//         return e.json();
+//     }).then(r => {
+//         fetch(r.properties.forecast).then(e =>{return e.json();}).then(r => {
+//             document.querySelector("#today > h3").innerHTML = new Date().toDateString();
+//             for(var i = 0; i < 3; i++){
+//                 document.getElementById("t" + (i + 1)).innerHTML = r.properties.periods[i].name;
+//                 document.getElementById("t" + (i + 1) + "symbol").innerHTML = getSymbol(r.properties.periods[i].shortForecast);
+//                 document.getElementById("t" +  (i + 1) + "forecast").innerHTML = r.properties.periods[i].shortForecast;
+//                 document.getElementById("t" + (i + 1) + "temp").innerHTML = "Temperature: " + r.properties.periods[i].temperature + r.properties.periods[i].temperatureUnit;
+//                 document.getElementById("t" + (i + 1) + "wind").innerHTML = "Wind: " + r.properties.periods[i].windSpeed  + " " + r.properties.periods[i].windDirection;
+//                 document.getElementById("t" + (i + 1) + "desc").innerHTML = r.properties.periods[i].detailedForecast;
+//                 console.log(i);
+//             }
+//         });
+//     });
+// }
+
+function displayToday(){
+    fetch(data).then(e =>{
+        return e.json();
+    }).then(r => {
+        fetch(r.properties.forecast).then(e =>{return e.json();}).then(r => {
+            var sec = document.createElement("section");
+            sec.id = "today";
+            var html = `<h2>Today</h2><h3>${new Date().toDateString()}</h3>`;
+            for(var i = 0; i < 3; i++){
+                html += `<section id="t${i}">`
+                html += `<h4 id="t${i}">${r.properties.periods[i].name}</h4>`;
+                html += `<p class="symbol" id="t${i}symbol"> ${getSymbol(r.properties.periods[i].shortForecast)}</p>`;
+                html += `<p id="t${i}forecast">${r.properties.periods[i].shortForecast}</p>`;
+                html += `<ul><li id="t${i}temp">Temperature: ${r.properties.periods[i].temperature + r.properties.periods[i].temperatureUnit} </li> 
+                <li id="t${i}wind">Wind: ${r.properties.periods[i].windSpeed  + " " + r.properties.periods[i].windDirection}</li></ul>`;
+                html += `<h5>Detailed Description:</h5> <p id="t${i}desc">${r.properties.periods[i].detailedForecast}</p>`;
+                html += `</section>`
+            }
+            sec.innerHTML = html;
+            document.getElementById("info").appendChild(sec);
+        });
+    });
+}
+
+
+function getSymbol(shortForecast){
+    if(shortForecast.toLowerCase().includes("thunderstorm")) return "&#x26C8;&#xFE0E;";
+    if(shortForecast.toLowerCase().includes("showers")&& shortForecast.toLowerCase().includes("chance")) return "🌦&#xFE0E;";
+    if(shortForecast.toLowerCase().includes("showers")) return "🌧";
+    if(shortForecast.toLowerCase().includes("cloudy") && shortForecast.toLowerCase().includes("partly")) return "⛅";
+    if(shortForecast.toLowerCase().includes("cloudy")) return "☁";
+
+    return "☀";
+
+}
+
+function clickToday(){
+    document.getElementById("info").style.display = "block";
+    document.getElementById("hourly").style.display = "none";
+    document.getElementById("weekly").style.display = "none";
+    displayToday();
+}
+
+function clickWeekly(){
+    document.getElementById("info").style.display = "none";
+    document.getElementById("hourly").style.display = "none";
+    document.getElementById("weekly").style.display = "block";
+    displayWeekly();
+}
+
+function displayWeekly(){
+    fetch(data).then(e =>{
+        return e.json();
+    }).then(r => {
+        fetch(r.properties.forecast).then(e =>{return e.json();}).then(r => {
+            var html = `<h2>Weekly</h2> <table id="weeklyTable"> <tr>`;
+            var periods = r.properties.periods;
+            for(var i = 0; i < periods.length; i++){
+                if(periods[i].name.includes("Night")) continue;
+                html += `<th>${periods[i].name}</th>`;
+            }
+            html += "</tr><tr>";
+            for(var i = 0; i < periods.length; i++){
+                if(periods[i].name.includes("Night")) continue;
+                html += `<td>${getSymbol(periods[i].shortForecast)}</td>`;
+            }
+            html +="</tr><tr>";
+            for(var i = 0; i < periods.length; i++){
+                if(periods[i].name.includes("Night")) continue;
+                html += `<td>${periods[i].temperature + periods[i].temperatureUnit}</td>`;
+            }
+            html +="</tr></table>";
+            document.getElementById("weekly").innerHTML = html;
+        });
+    });
+}
+
+function getData(){
+    var b = window.location.search.replace("?", "").split("&");
+    var c = [];
+    for(var i in b){
+        var d = b[i].split("=");
+        c.push([d[0], d[1]]);
+    }
+    return c;
+}
